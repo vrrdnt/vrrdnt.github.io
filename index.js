2e175637df7486d1773821fb746a126d53ce92ed
vrrdnt <will.arctic@gmail.com>
2024-07-15 23:08:49 +0900

    docker stuff

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 4060;
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, public)));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Start the server
 app.listen(PORT, () => {
